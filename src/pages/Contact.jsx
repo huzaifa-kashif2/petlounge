b00ca9c8b1d71a13d8bc94db0ebf7c2a7bfd2ad6
huzaifa-kashif2 <huzaifa.kashif2@gmail.com>
2025-08-22 13:22:38 +0500

    Changes Done as per client

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -67,7 +67,7 @@ export default function Contact() {
       </div>
       <div className={styles.mapContainer}>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6805.348647515624!2d74.40136627763954!3d31.47814386923759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391908ab93291a45%3A0xdaea234f9647e517!2sSector%20V%20DHA%20Phase%202%2C%20Lahore%2C%20Pakistan!5e0!3m2!1sen!2s!4v1754405397981!5m2!1sen!2s"
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3402.578895788739!2d74.40953447561002!3d31.480768174232047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDI4JzUwLjgiTiA3NMKwMjQnNDMuNiJF!5e0!3m2!1sen!2s!4v1755850643391!5m2!1sen!2s" 
           width="100%"
           height="300"
           style={{ border: 0 }}
